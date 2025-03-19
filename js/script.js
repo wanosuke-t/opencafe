81ab04599fdf15dfca33933d031860f9cb5f3956
wanosuke <contact@wanosuke.jp>
2025-03-19 18:15:45 +0900

    First commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// ドロワーメニュー
+$("#js-drawer-icon").on("click", function () {
+  $(this).toggleClass("is-checked");
+  $("#js-drawer-content").toggleClass("is-checked");
+  $("#js-drawer").toggleClass("is-checked");
+});
+
+// スマホのドロワーアイコンを消す処理
+$('#js-drawer-content a[href^="#"]').on("click", function (e) {
+  $("#js-drawer-icon").removeClass("is-checked");
+  $("#js-drawer-content").removeClass("is-checked");
+  $("#js-drawer").removeClass("is-checked");
+});
+
+//ドロワーの外側をクリックしたらモーダルを閉じる
+$(document).on("click", function (e) {
+  // クリックした要素がドロワーの中またはトリガーなら何もしない
+  if ($(e.target).closest("#js-drawer-content, #js-drawer-icon").length) {
+    return;
+  }
+
+  // ドロワーを閉じる
+  $("#js-drawer-icon").removeClass("is-checked");
+  $("#js-drawer-content").removeClass("is-checked");
+  $("#js-drawer").removeClass("is-checked");
+});
+
+// /* drawer */
+// jQuery('.drawer-icon').on('click', function () {
+// 	jQuery('.drawer').toggleClass('m_checked');
+// });
+
+// jQuery(window).on('scroll', function ($) {
+// 	if (100 < jQuery(this).scrollTop()) {
+// 		jQuery('.floating').show();
+// 	} else {
+// 		jQuery('.floating').hide();
+// 	}
+// });
+
+// /* SmoothScroll */
+// jQuery('a[href^="#"]').click(function () {
+// 	var header = 0; // jQuery( '#header' ).height();
+// 	var speed = 300;
+// 	var id = jQuery(this).attr('href');
+// 	var target = jQuery('#' == id ? 'html' : id);
+// 	var position = jQuery(target).offset().top - header;
+// 	if (0 > position) {
+// 		position = 0;
+// 	}
+// 	jQuery('html, body').animate(
+// 		{
+// 			scrollTop: position
+// 		},
+// 		speed
+// 	);
+// 	return false;
+// });
