b7abd1a54401e644b3d0208dc073b7f041641575
wanosuke <contact@wanosuke.jp>
2025-03-23 05:34:44 +0900

    Add p-page-header

--- js/script.js
+++ js/script.js
@@ -6,9 +6,8 @@ $("#js-drawer-icon").on("click", function () {
 });
 
 const drawer = document.querySelector("#js-drawer");
-
 window.addEventListener("scroll", function () {
-  if (window.scrollY > 200) {
+  if (window.scrollY > 250) {
     drawer.classList.add("is-show");
   } else {
     drawer.classList.remove("is-show");
@@ -57,43 +56,10 @@ const swiper = new Swiper(".swiper", {
 
 // ページトップボタン
 const pageTop = document.querySelector("#js-pagetop");
-
 window.addEventListener("scroll", function () {
-  if (window.scrollY > 100) {
+  if (window.scrollY > 250) {
     pageTop.classList.add("is-show");
   } else {
     pageTop.classList.remove("is-show");
   }
 });
-
-// /* drawer */
-// jQuery('.drawer-icon').on('click', function () {
-// 	jQuery('.drawer').toggleClass('m_checked');
-// });
-
-// jQuery(window).on('scroll', function ($) {
-// 	if (100 < jQuery(this).scrollTop()) {
-// 		jQuery('.floating').show();
-// 	} else {
-// 		jQuery('.floating').hide();
-// 	}
-// });
-
-// /* SmoothScroll */
-// jQuery('a[href^="#"]').click(function () {
-// 	var header = 0; // jQuery( '#header' ).height();
-// 	var speed = 300;
-// 	var id = jQuery(this).attr('href');
-// 	var target = jQuery('#' == id ? 'html' : id);
-// 	var position = jQuery(target).offset().top - header;
-// 	if (0 > position) {
-// 		position = 0;
-// 	}
-// 	jQuery('html, body').animate(
-// 		{
-// 			scrollTop: position
-// 		},
-// 		speed
-// 	);
-// 	return false;
-// });
